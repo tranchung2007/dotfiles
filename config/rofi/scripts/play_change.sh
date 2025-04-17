e18c017bc6e505f40ceac21371a55382295de49e
tranchung2007 <166964670+tranchung2007@users.noreply.github.com>
2025-04-18 06:26:50 +0700

    Delete config directory

--- config/rofi/scripts/play_change.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#echo "$@" >> $HOME/selection_log
-
-#if [[ -n "$@" ]]; then 
-    #killall espeak
-    #echo "selected: $@" | sed -e 's/<span.*<\/span>//g' | espeak
-    coproc aplay -q /home/mat/Programs/rofi-desktop/scripts/sounds/current/change.wav
-#fi
