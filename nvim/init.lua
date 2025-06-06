7d2d4c9e015d7118e14f23eb382f3a7e750d87af
Chung Tran <banalover2ten@gmail.com>
2025-06-06 20:38:41 +0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,62 +27,62 @@ vim.keymap.set("n", "<leader>b", "<cmd>cgetb | bd! | cope<CR>", { silent = true,
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	spec = {
-		{ "miikanissi/modus-themes.nvim", priority = 1000 },
-		{
-			"stevearc/oil.nvim",
-			---@module 'oil'
-			---@type oil.SetupOpts
-			opts = {
-				vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
-				constrain_cursor = "name",
-				columns = {
-					"permissions",
-					"birthtime",
-					"size",
-				},
-			},
-			lazy = false,
-		},
-		{
-			"dense-analysis/ale",
-			config = function()
-				vim.g.ale_completion_enabled = 1
-				vim.cmd("set omnifunc=ale#completion#OmniFunc")
-				vim.g.ale_fixers = {
-					["*"] = { "remove_trailing_lines", "trim_whitespace" },
-				}
-			end,
-		},
-		{
-			"nvim-treesitter/nvim-treesitter",
-			lazy = false,
-			build = ":TSUpdate",
-			config = function()
-				require("nvim-treesitter.configs").setup({
-					auto_install = true,
-					highlight = {
-						enable = true,
-					},
-					incremental_selection = {
-						enable = true,
-						keymaps = {
-							init_selection = "<C-space>",
-							node_incremental = "<C-space>",
-							scope_incremental = "<A-space>",
-							node_decremental = "<bs>",
-						},
-					},
-				})
-			end,
-		},
-	},
+    spec = {
+        { "miikanissi/modus-themes.nvim", priority = 1000 },
+        {
+            "stevearc/oil.nvim",
+            ---@module 'oil'
+            ---@type oil.SetupOpts
+            opts = {
+                vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+                constrain_cursor = "name",
+                columns = {
+                    "permissions",
+                    "birthtime",
+                    "size",
+                },
+            },
+            lazy = false,
+        },
+        {
+            "dense-analysis/ale",
+            config = function()
+                vim.g.ale_completion_enabled = 1
+                vim.cmd("set omnifunc=ale#completion#OmniFunc")
+                vim.g.ale_fixers = {
+                    ["*"] = { "remove_trailing_lines", "trim_whitespace" },
+                }
+            end,
+        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            lazy = false,
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    auto_install = true,
+                    highlight = {
+                        enable = true,
+                    },
+                    incremental_selection = {
+                        enable = true,
+                        keymaps = {
+                            init_selection = "<C-space>",
+                            node_incremental = "<C-space>",
+                            scope_incremental = "<A-space>",
+                            node_decremental = "<bs>",
+                        },
+                    },
+                })
+            end,
+        },
+    },
 })
 
 vim.cmd.colorscheme("modus_vivendi")
