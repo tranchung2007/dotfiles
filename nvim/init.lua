5c299313cec93dd84994e1cfaf55fd477b9eba39
Chung Tran <banalover2ten@gmail.com>
2025-06-06 20:03:37 +0700

    using lazy.nvim, update

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,7 @@ vim.o.cinoptions = "l1"
 vim.o.undofile = true
 vim.opt.completeopt = { "noselect", "noinsert", "menu" }
 vim.cmd("set clipboard+=unnamedplus")
-vim.diagnostic.config({ virtual_text = { current_line = true }})
+vim.diagnostic.config({ virtual_text = { current_line = true } })
 
 vim.keymap.set("n", "<leader>r", ":sp | term ", { noremap = true })
 vim.keymap.set("n", "<leader>ff", ":sp | term rg --vimgrep --files | rg ", { noremap = true })
@@ -25,35 +25,64 @@ vim.keymap.set("n", "<leader>fw", ":sp | term rg --vimgrep ", { noremap = true }
 vim.keymap.set("n", "<leader>c", "<cmd>bd!<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>b", "<cmd>cgetb | bd! | cope<CR>", { silent = true, noremap = true })
 
-local vim = vim
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
-    Plug('miikanissi/modus-themes.nvim')
-    Plug('stevearc/oil.nvim')
-    Plug('dense-analysis/ale')
-    Plug('nvim-treesitter/nvim-treesitter')
-vim.call('plug#end')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-require("oil").setup({
-    constrain_cursor = "name",
-    columns = {
-        "permissions",
-        "birthtime",
-        "size",
-    },
+require("lazy").setup({
+	spec = {
+		{ "miikanissi/modus-themes.nvim", priority = 1000 },
+		{
+			"stevearc/oil.nvim",
+			---@module 'oil'
+			---@type oil.SetupOpts
+			opts = {
+				vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+				constrain_cursor = "name",
+				columns = {
+					"permissions",
+					"birthtime",
+					"size",
+				},
+			},
+			lazy = false,
+		},
+		{
+			"dense-analysis/ale",
+			config = function()
+				vim.g.ale_completion_enabled = 1
+				vim.cmd("set omnifunc=ale#completion#OmniFunc")
+				vim.g.ale_fixers = {
+					["*"] = { "remove_trailing_lines", "trim_whitespace" },
+				}
+			end,
+		},
+		{
+			"nvim-treesitter/nvim-treesitter",
+			lazy = false,
+			build = ":TSUpdate",
+			config = function()
+				require("nvim-treesitter.configs").setup({
+					auto_install = true,
+					highlight = {
+						enable = true,
+					},
+					incremental_selection = {
+						enable = true,
+						keymaps = {
+							init_selection = "<C-space>",
+							node_incremental = "<C-space>",
+							scope_incremental = "<A-space>",
+							node_decremental = "<bs>",
+						},
+					},
+				})
+			end,
+		},
+	},
 })
--- require("nvim-treesitter").setup({})
 
--- vim.lsp.config['clangd'] = {
---     cmd = { 'clangd', '--header-insertion=never' },
---     filetypes = { 'c', 'cpp', 'h', 'hpp' },
--- }
--- vim.lsp.enable('clangd')
-
-vim.cmd.colorscheme('modus_vivendi')
-
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_node_provider = 0
+vim.cmd.colorscheme("modus_vivendi")
